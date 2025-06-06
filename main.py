96a53e3d64c3989cb83f12883a22d8600876b1c3
Amber Nguyen <ambernguyen5@berkeley.edu>
2025-06-06 16:21:52 -0700

    added title and tags for prototyping on SwaggerUI

--- main.py
+++ main.py
@@ -2,11 +2,6 @@
 from fastapi import FastAPI
 from routes import blood_pressure
 
-app = FastAPI()
-app.include_router(blood_pressure.router)
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
-
-
+app = FastAPI(title="Health Monitoring API",
+              description="Moniotors blood pressure data")
+app.include_router(blood_press
